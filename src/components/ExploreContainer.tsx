0e37bbffb1e003ca82d3980c0f1346fff8ceb50d
Natalia Melikyants <melikyants@gmail.com>
2020-12-02 14:28:42 +0100

    change titles

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -6,8 +6,7 @@ interface ContainerProps { }
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <strong>Welcome to Bellwether Coffee</strong>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,13 +8,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Bellwether Coffee</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank Test</IonTitle>
+            <IonTitle size="large">Bellwether Coffee</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
